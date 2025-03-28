812b03a570665e6204ff485ae35f7721b796ed53
Alireza <rostmi83@gmail.com>
2025-03-28 11:05:11 +0330

    add document class

---
+++ src/example/Document.java
@@ -0,0 +1,40 @@
+package example;
+
+import db.Entity;
+import db.Trackable;
+
+import java.util.Date;
+
+public class  Document extends Entity implements Trackable {
+
+    String content;
+
+    public Document (String content){
+        this.content = content;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return 0;
+    }
+
+    @Override
+    public void setCreationDate(Date date) {
+
+    }
+
+    @Override
+    public Date getCreationDate() {
+        return null;
+    }
+
+    @Override
+    public void setLastModificationDate(Date date) {
+
+    }
+
+    @Override
+    public Date getLastModificationDate() {
+        return null;
+    }
+}
